3095d8b8772c241affe7702e110383d6c1a2bf05
rajeshwar.patlolla <rajeshwar.patlolla@gmail.com>
2025-05-13 18:45:38 +0530

    Converted from CommonJS to ES Modules and updated a few packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,4 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-];
+export default [{ files: ['**/*.js'], languageOptions: { sourceType: 'module' } }, { languageOptions: { globals: globals.node } }, pluginJs.configs.recommended];
